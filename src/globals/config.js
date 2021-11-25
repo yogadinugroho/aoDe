fa811f2f6b0c42cb59204b8c114651c994cc6f36
Yoga Adi Nugroho <78912216+yogadinugroho@users.noreply.github.com>
2021-11-25 22:19:05 +0700

    menambahkan cache name, db name, db version, dan object store name

--- src/globals/config.js
+++ src/globals/config.js
@@ -1,3 +1,8 @@
 const CONFIG = {
-    BASE_URL: 'https://doa-doa-api-ahmadramadhan.fly.dev/'
+    BASE_URL: 'https://doa-doa-api-ahmadramadhan.fly.dev/',
+    DEFAULT_LANGUAGE: 'en-us',
+    CACHE_NAME: aoDeCache,
+    DATABASE_NAME: 'aoDe-database',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'aoDe',
 }
