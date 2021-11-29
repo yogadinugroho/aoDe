248a2b33eda6c435ec2611d1a04a925a0352414f
yogadinugroho <yogaadi.work@gmail.com>
2021-11-29 21:34:36 +0700

    menambahkan css

--- src/scripts/components/header.js
+++ src/scripts/components/header.js
@@ -5,6 +5,76 @@ class CustomHeader extends HTMLElement {
 
   render() {
     this.innerHTML = `
+    <style>
+
+      .app-bar {
+        padding: 8px 16px;
+        background-color: white;
+        display: grid;
+        grid-template-columns: auto 1fr auto;
+        gap: 10px;
+        position: sticky;
+        top: 0;
+        z-index: 99;
+        box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2);
+      }
+
+      .app-bar .app-bar__menu {
+        display: flex;
+        align-items: center;
+      }
+
+      .app-bar .app-bar__menu button {
+        display: inline-block;
+        min-width: 44px;
+        min-height: 44px;
+        background-color: transparent;
+        border: none;
+        font-size: 18px;
+        padding: 8px;
+        cursor: pointer;
+      }
+
+      .app-bar .app-bar__brand {
+        display: flex;
+        align-items: center;
+      }
+
+      .app-bar .app-bar__brand h1 {
+        color: #db0000;
+        text-transform: uppercase;
+        font-size: 22px;
+        user-select: none;
+      }
+
+      .app-bar .app-bar__navigation {
+        position: absolute;
+        top: 50px;
+        left: -180px;
+        width: 150px;
+        transition: all 0.3s;
+        padding: 8px;
+        background-color: white;
+        overflow: hidden;
+      }
+
+      .app-bar .app-bar__navigation.open {
+        left: 0;
+      }
+
+      .app-bar .app-bar__navigation ul li a {
+        display: inline-block;
+        text-decoration: none;
+        color: black;
+        padding: 8px;
+        margin-bottom: 5px;
+        width: 100%;
+        min-width: 44px;
+        min-height: 44px;
+      }
+      
+    </style>
+
     <header class="app-bar">
         <div class="app-bar__menu">
                 <button id="hamburgerButton">☰</button>
