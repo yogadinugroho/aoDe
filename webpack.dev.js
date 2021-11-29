d4c0749d947a655575d0df1890b2936bbb970270
yogadinugroho <yogaadi.work@gmail.com>
2021-11-29 20:48:46 +0700

    konfigurasi webpack

---
+++ webpack.dev.js
@@ -0,0 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
+const { merge } = require('webpack-merge');
+const path = require('path');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+  },
+});
