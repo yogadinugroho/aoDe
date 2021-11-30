dcc7ff9270682a040cded7043be98e7c2ba7b731
yogadinugroho <yogaadi.work@gmail.com>
2021-12-01 01:37:40 +0700

    tambah gambar untuk trial, perubahan api dan endPt

--- src/scripts/globals/api-endpoint.js
+++ src/scripts/globals/api-endpoint.js
@@ -1,6 +1,9 @@
 import CONFIG from './config';
 
 const API_ENDPOINT = {
-    LIST_DOA: `${CONFIG.BASE_URL}/api`,
-    SPESIFIK_DOA: (id) => `${CONFIG.BASE_URL}/api/${id}`,
-}
+  LIST_DOA: `${CONFIG.BASE_URL}/all`,
+  SPESIFIK_DOA: (keywords) => `${CONFIG.BASE_URL}/search/${keywords}`,
+  DETAIL_DOA: (id) => `${CONFIG.BASE_URL}/show/${id}`,
+};
+
+export default API_ENDPOINT;
--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
-    BASE_URL: 'https://doa-doa-api-ahmadramadhan.fly.dev/',
-    DEFAULT_LANGUAGE: 'en-us',
-    CACHE_NAME: aoDeCache,
-    DATABASE_NAME: 'aoDe-database',
-    DATABASE_VERSION: 1,
-    OBJECT_STORE_NAME: 'aoDe',
-}
+  BASE_URL: 'https://tanyadoa-api.herokuapp.com',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'aoDeCache',
+  DATABASE_NAME: 'aoDe-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'aoDe',
+};
 
 export default CONFIG;
