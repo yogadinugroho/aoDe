98170acaecad7b03c5051cde401caab72a2cf20e
rifqiarrahim <mrifqian@gmail.com>
2021-12-03 23:12:08 +0700

    final look btn-card

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -18,26 +18,26 @@ const createDetailDoaItemTemplate = (doa) => `
     <div class="card mx-auto card-detail">
         <img src="https://png.pngtree.com/element_our/20190529/ourlarge/pngtree-cartoon-sleeping-day-character-illustration-image_1216735.jpg" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Lafal :</h5>
+            <h3 class="card-title">Lafal :</h3>
             <p class="card-text" align="justify">${doa.lafal}</p>
         </div>
         <div class="card-body">
-            <h5 class="card-title">Transliterasi :</h5>
+            <h3 class="card-title">Transliterasi :</h3>
             <p class="card-text" align="justify">${doa.transliterasi}</p>
         </div>
         <div class="card-body">
-            <h5 class="card-title">Artinya :</h5>
+            <h3 class="card-title">Artinya :</h3>
             <p class="card-text" align="justify">${doa.arti}</p>
         </div>
         <div class="card-body">
-            <h5 class="card-title">Sumber :</h5>
+            <h3 class="card-title">Sumber :</h3>
             <p class="card-text" align="justify">${doa.riwayat}</p>
         </div>
         <div class="container-button">
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                <button class="btn btn-primary" type="button">Button</button>
-                <button class="btn btn-primary" type="button">Button</button>
-                <button class="btn btn-primary" type="button">Button</button>
+                <span class="material-icons-outlined btn-card">volume_up</span>
+                <span class="material-icons-round btn-card">auto_stories</span>
+                <span class="material-icons-round btn-card">psychology</span>
             </div>
         </div>
     </div>
